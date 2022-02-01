4fdd39dc3034e4d589a7b7c1c12109dbb8e40594
andrezzz2312 <andrezzz2312@gmail.com>
2022-01-31 22:13:10 -0500

    .

--- src/components/LanguageButton.js
+++ src/components/LanguageButton.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import App from './App';
 import {FaBeer} from 'react-icons/fa';
+import mexico from '../assets/mx.png';
 
 export default function LanguageButton() {
   return (
     <div className='languangeButton'>
       <div className='languageButton-wrapper'>
-        <img src='/mx4.3.svg' alt='SVG as an image' />
+        <img className='icon' src={mexico} />
       </div>
     </div>
   );
